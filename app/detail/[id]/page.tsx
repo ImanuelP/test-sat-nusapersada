8a3b11f8daad41bf74a8783dcbd0e913e3bfe76d
Imanuel Elkana <imanuel.elkana@pmberjaya.com>
2024-05-03 18:07:53 +0700

    update

---
+++ app/detail/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <div className="flex px-4 justify-center dark:bg-black">
+      <div> 
+        Test
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import { getServer } from "@/api/getServer";
 import { DataPosts } from "@/interface";
+import Link from "next/link";
 
 export default async function Home() {
   const data: DataPosts[] = await getServer('posts')
-  console.log(data)
   return (
-    <div className="">
-      {data.map((row) => (
-        <div key={row.id}>{row.title}</div>
-      ))}
+    <div className="flex px-4 justify-center dark:bg-black">
+      <div>
+        {data.map((row) => (
+          <Link key={row.id} href={`/detail/${row.id}`}>
+            <div className="sm:w-[500px] transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 border-2 rounded-lg border-blue-400 dark:border-white p-4 my-4 cursor-pointer">
+              <div className="text-xl text-blue-700 dark:text-blue-400  capitalize hover:underline font-semibold">{row.title}</div>
+              <div className="text-sm">{row.body}</div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
